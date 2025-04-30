f237ab11a836538113793b94f8fb6e28a9a650d6
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-04-30 11:16:03 +0100

    main flex center

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-//npm install -D webpack webpack-cli html-loader html-webpack-plugin css-loader style-loader webpack-dev-server webpack-merge
+//npm install -D webpack webpack-cli file-loader html-loader html-webpack-plugin css-loader style-loader webpack-dev-server webpack-merge
 
 import './styles.css';
 import './styles_signs.css';
 
 import './modules/global_module';
+import ranniWelcome from '../ressources/audio/ranniWelcome.wav';
 
-// let ranniWellcome = new Audio('src\ranni-the-witch-tts-file.wav');
+// const playButton = document.querySelector('.factionImage');
 
-// setTimeout(() => {
-//   ranniWellcome.play();
-// }, 1000);
+// const audio = new Audio(ranniWelcome);
+// console.log(audio);
+// playButton.addEventListener('click', () => {
+//   audio.play();
+// });
--- webpack.common.js
+++ webpack.common.js
@@ -34,6 +34,27 @@ module.exports = {
         test: /\.(woff|woff2|ttf|otf|eot)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: 'defaults',
+            presets: [['@babel/preset-env']],
+          },
+        },
+      },
+      {
+        test: /\.(wav|mp3)$/i,
+        type: 'asset/resource',
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'media', // Optional: Output audio files to a 'media' folder
+          publicPath: 'media', // Optional: Ensure correct public path in the browser
+        },
+      },
     ],
   },
 };
