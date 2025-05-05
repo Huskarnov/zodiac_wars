8b5f2d600fce592a39f8770b24a7eb5ee7eb311a
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-05-05 20:29:03 +0100

    x

--- src/modules/battle_interface_module.js
+++ src/modules/battle_interface_module.js
@@ -39,91 +39,95 @@ summonEmpower.addEventListener('mouseleave', (event) => {
 //   attackingHealth,
 //   attackingDamage
 function setBattleInterface() {
-  switch (stars[selectedConstellation][selectedStar].defending) {
-    case 'aries':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/virgo1.webp)';
-      break;
-    case 'taurus':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'gemini':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'cancer':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'leo':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'virgo':
-      defender.style.backgroundImage =
-        'url(./ressources/images/avatars/virgo/virgo1.webp)';
-      break;
-    case 'libra':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'scorpio':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'sagittarius':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'capricorn':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'aquarius':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case 'pisces':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-    case '':
-      defender.style.backgroundImage =
-        'url(/ressources/images/avatars/virgo/taurus1.webp)';
-      break;
-  }
+  defender.style.backgroundImage =
+    'url(./ressources/images/avatars/virgo/virgo1.webp)';
+  // document.body.style.backgroundImage = "url('img_tree.png')";
+
+  // switch (stars[selectedConstellation][selectedStar].defending) {
+  //   case 'aries':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/virgo1.webp)';
+  //     break;
+  //   case 'taurus':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'gemini':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'cancer':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'leo':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'virgo':
+  //     defender.style.backgroundImage =
+  //       'url(./ressources/images/avatars/virgo/virgo1.webp)';
+  //     break;
+  //   case 'libra':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'scorpio':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'sagittarius':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'capricorn':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'aquarius':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case 'pisces':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  //   case '':
+  //     defender.style.backgroundImage =
+  //       'url(/ressources/images/avatars/virgo/taurus1.webp)';
+  //     break;
+  // }
 
   if (stars[selectedConstellation][selectedStar].peace === true) {
     attacker.style.display = 'none';
   } else if (stars[selectedConstellation][selectedStar].peace === false) {
-    attacker.style.display = 'block';
+    attacker.style.display = 'flex';
   }
 
   battleStar.textContent = stars[selectedConstellation][selectedStar].name;
   dominion.textContent = `Dominion: ${stars[selectedConstellation][selectedStar].defending}`;
-  defenderName.textContent =
-    stars[selectedConstellation][selectedStar].defending;
-  defenderHp.textContent =
-    stars[selectedConstellation][selectedStar].defendingHealth;
-  defenderDmg.textContent =
-    stars[selectedConstellation][selectedStar].defendingDamage;
+  // defenderName.textContent =
+  //   stars[selectedConstellation][selectedStar].defending;
+  // defenderHp.textContent =
+  //   stars[selectedConstellation][selectedStar].defendingHealth;
+  // defenderDmg.textContent =
+  //   stars[selectedConstellation][selectedStar].defendingDamage;
 
-  attackerName.textContent =
-    stars[selectedConstellation][selectedStar].attacking;
-  attackingHp.textContent =
-    stars[selectedConstellation][selectedStar].attackingHealth;
-  attackingDmg.textContent =
-    stars[selectedConstellation][selectedStar].attackingDamage;
+  // attackerName.textContent =
+  //   stars[selectedConstellation][selectedStar].attacking;
+  // attackingHp.textContent =
+  //   stars[selectedConstellation][selectedStar].attackingHealth;
+  // attackingDmg.textContent =
+  //   stars[selectedConstellation][selectedStar].attackingDamage;
 
   if (
     stars[selectedConstellation][selectedStar].defending ===
       users.me.zodiacSign ||
     stars[selectedConstellation][selectedStar].attacking === users.me.zodiacSign
   ) {
-    summonEmpowerButton.textContent = 'Empower Avatar';
+    summonEmpowerButton.textContent = 'Empower our Avatar';
   } else {
-    summonEmpowerButton.textContent = 'Summon Avatar';
+    summonEmpowerButton.textContent = 'Summon our Avatar';
   }
 
   hpValue.textContent = users.me.health;
